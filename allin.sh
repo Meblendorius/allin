30cf3f3745d8786baa25c85b5ceb62056b43b1ed
Meblendorius <69820416+Meblendorius@users.noreply.github.com>
2022-01-07 15:02:06 -0300

    Update allin.sh

--- allin.sh
+++ allin.sh
@@ -7,7 +7,7 @@ echo "Servidor";
 whois $* | grep -E 'nserver|status';
 echo "";
 echo "Ambiente";
-ping -c 1 webmail.$* | grep PING
-ping -c 1 pop.$* | grep POP
-ping -c 1 imap.$* | grep IMAP
+ping -c 1 webmail.$* | grep PING;
+ping -c 1 pop.$* | grep POP;
+ping -c 1 imap.$* | grep IMAP;
 ping -c 1 smtp.$* | grep SMTP
