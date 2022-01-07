24925fd8aa24c65a044d4ec660fa1d7259a57cab
Meblendorius <69820416+Meblendorius@users.noreply.github.com>
2022-01-07 14:29:55 -0300

    Update allin.sh

--- allin.sh
+++ allin.sh
@@ -8,3 +8,6 @@ whois $* | grep -E 'nserver|status';
 echo "";
 echo "Ambiente";
 ping -c 1 webmail.$* | grep PING
+ping -c 1 pop.$* | grep POP
+ping -c 1 imap.$* | grep IMAP
+ping -c 1 smtp.$* | grep SMTP
